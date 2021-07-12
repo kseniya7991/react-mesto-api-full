ef21e73a6b219b2c5aafd5779d0d6ccf16bb9ea3
Kseniya St <kseniya7991@gmail.com>
2021-07-12 06:51:28 +0300

    move logs to folder 'log'

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -4,7 +4,7 @@ const expressWinston = require('express-winston');
 // логгер запросов
 const requestLogger = expressWinston.logger({
   transports: [
-    new winston.transports.File({ filename: 'request.log' }),
+    new winston.transports.File({ filename: '/logs/request.log' }),
   ],
   format: winston.format.json(),
 });
@@ -12,7 +12,7 @@ const requestLogger = expressWinston.logger({
 // логгер ошибок
 const errorLogger = expressWinston.errorLogger({
   transports: [
-    new winston.transports.File({ filename: 'error.log' }),
+    new winston.transports.File({ filename: '/logs/error.log' }),
   ],
   format: winston.format.json(),
 });

0e0ea9a24ac289653f9973c07f040ad1923c82c7
Kseniya St <kseniya7991@gmail.com>
2021-07-12 06:59:52 +0300

    add limiter to project

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ const app = express();
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 2, // limit each IP to 100 requests per windowMs
+  max: 100, // limit each IP to 100 requests per windowMs
 });
 
 const methodValidation = (value) => {

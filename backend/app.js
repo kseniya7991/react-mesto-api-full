2a362404e288e91b3ab82fc1abfda8f7025c516b
Kseniya St <kseniya7991@gmail.com>
2021-07-12 19:13:19 +0300

    add limiter

--- backend/app.js
+++ backend/app.js
@@ -21,11 +21,11 @@ const app = express();
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 15 minutes
-  max: 3, // limit each IP to 100 requests per windowMs
+  max: 10, // limit each IP to 100 requests per windowMs
   message: 'Too many accounts created from this IP, please try again after an hour',
 });
 
-app.set('trust proxy', true);
+app.set('trust proxy', 1);
 
 app.use(limiter);
 

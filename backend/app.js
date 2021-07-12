b12d9d3265e1c1e9239ec08b39a22df840865057
Kseniya St <kseniya7991@gmail.com>
2021-07-12 19:05:33 +0300

    add limiter

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const rateLimit = require('express-rate-limit');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -17,6 +18,15 @@ const { createUser, login } = require('./controllers/user');
 const handleErrors = require('./handle-errors');
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 10000, // 15 minutes
+  max: 800, // limit each IP to 100 requests per windowMs
+});
+
+app.set('trust proxy', true);
+
+app.use(limiter);
 
 const methodValidation = (value) => {
   const correctLink = validator.isURL(value, { require_protocol: true });

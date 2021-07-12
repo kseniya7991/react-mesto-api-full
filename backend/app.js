375ea04da5c9a9cb6e68709af825a1a4f626c1b6
Kseniya St <kseniya7991@gmail.com>
2021-07-12 18:49:44 +0300

    add limiter

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-/* const rateLimit = require('express-rate-limit'); */
+const rateLimit = require('express-rate-limit');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -18,11 +18,11 @@ const { createUser, login } = require('./controllers/user');
 const handleErrors = require('./handle-errors');
 
 const app = express();
-/*
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 800, // limit each IP to 100 requests per windowMs
-}); */
+});
 
 const methodValidation = (value) => {
   const correctLink = validator.isURL(value, { require_protocol: true });
@@ -67,8 +67,8 @@ app.use(cookieParser('secret'));
 
 app.use(requestLogger); // подключаем логгер запросов
 
-/* app.use(limiter);
- */
+app.use(limiter);
+
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
